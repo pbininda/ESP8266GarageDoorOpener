e9d6e71136d61190fce0a8690a7b9954f30df7e7
Paul Bininda <paul@bininda.com>
2017-02-10 18:17:28 +0100

    Initial commit

---
+++ ESP8266GarageDoorOpener.ino
@@ -0,0 +1,125 @@
+#include <RFControl.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+#ifndef RF_CONTROL_VARDUINO
+#include "arduino_functions.h"
+#endif
+
+extern "C" {
+  #include "user_interface.h"
+}
+
+#include "SSID_PASSWORD.h"
+
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
+
+ESP8266WebServer server(80);
+const bool DEBUG_HTTP = 0;
+long commandsSent = 0;
+const int pin = 2;
+
+#define TITEL "Garagenschalter"
+
+void initWiFi() {
+  // Connect to WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  WiFi.setSleepMode(WIFI_LIGHT_SLEEP);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
+}
+
+
+
+
+void loop() {
+  server.handleClient();
+  wifi_set_sleep_type(LIGHT_SLEEP_T);
+  delay(200);
+}
+
+String head() {
+  return "<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<meta content=\"text/html; charset=ISO-8859-1\" http-equiv=\"content-type\">\r\n<title>" TITEL "</title>\r\n</hread>\r\n<body>\r\n";
+}
+
+String tail() {
+  return "</body>\r\n</html>\r\n";
+}
+
+void sendResult(String &resp) {
+  server.send(200, "text/html", resp);
+}
+
+String statusBody() {
+  String res = "<p>\r\n";
+  res += "Commands sent: ";
+  res += commandsSent;
+  res += "\r\n</p>\r\n";
+  return res;
+}
+
+String buttonBody() {
+  return "<form action=\"/\" method=\"post\"><button type=\"submit\">Switch</button></form>\r\n";
+}
+
+void handleIndex() {
+  sendResult(head() + statusBody() + buttonBody() + tail());
+}
+
+void handleSwitch() {
+  garageSend();
+  sendResult(head() + "<p>sent command</p>\r\n" + statusBody() + buttonBody() + tail());
+}
+
+void initServer() {
+  // Start the server
+  server.on("/", HTTP_GET, handleIndex);
+  server.on("/", HTTP_POST, handleSwitch);
+  server.on("/switch", HTTP_GET, handleSwitch);
+  server.begin();
+  Serial.print("Server started on ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void garageSend() {
+  if (!DEBUG_HTTP) {
+    int pin = 2;
+    unsigned long int buckets[] = {404, 1184, 20508};
+    char pulses[] = "001010101011010101001100110100101011001100110100101010101010101102";
+    unsigned int repeats = 30;
+    RFControl::sendByCompressedTimings(pin, buckets, pulses, repeats);  
+  }
+  commandsSent++;
+}
+
+void init433() {
+  hw_pinMode(pin, OUTPUT);
+  hw_digitalWrite(pin, LOW);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+  Serial.println("initializing 433MHz transmitter\r\n");
+  init433();
+  Serial.println("initializing WIFI\r\n");
+  initWiFi();
+  Serial.println("initializing server\r\n");
+  initServer();
+  Serial.println("ready for commands\r\n");
+}
+
+
---
+++ README.md
@@ -0,0 +1,62 @@
+# ESP 8266 Garage Door Opener
+
+Used to control a garage door with an ESP 8266 system.
+
+## Material
+
+### SunFounder ESP8266
+<img src="doc/assets/Esp8266_and_pin_.png"/>
+  * Amazon: https://www.amazon.de/gp/product/B00U293Y5M
+  * SunFounder wiki: https://www.sunfounder.com/wiki/index.php?title=Esp8266_Wifi_Module
+
+### Eastern Computer ca-5 FT232RL FTDI Micro USB to Serial Converter
+
+<img src="doc/assets/EasternFT232RL.jpg"/>
+
+* Amazon: https://www.amazon.de/gp/product/B00QCCPCNK
+
+### 3.3V Voltage Regulator Step-Down
+
+<img src="doc/assets/s-l1600.jpg"/>
+
+* ebay: http://www.ebay.de/itm/272466294878
+
+### Logic Level Converter
+
+<img src="doc/assets/41wG3-w76TL.jpg"/>
+
+* Amazon: https://www.amazon.de/gp/product/B01B3KY6Q4
+
+### 433MHz Receiver
+
+<img src="doc/assets/433RxTx.jpg">
+
+* ebay: http://www.ebay.de/itm/272414344399
+
+## Development Build
+
+See http://www.xess.com/blog/esp8266-reflash/ for a dev board setup.
+
+Note, since I didn't have a 3.3v USB to serial converter, I had to add the 3.3V voltage regulator.
+
+<img src="doc/assets/esp8266_flash_prog_board_sch.png"/>
+<img src="doc/assets/esp8266_flash_prog_board.jpg"/>
+
+
+## Final Build
+
+<img src="doc/assets/GDOFull.jpg">
+
+* Full module with antenna (&lambda;/4 ~ 17cm)
+
+<img src="doc/assets/GDOTopPopulated.jpg">
+
+* Pupulated board top view
+
+<img src="doc/assets/GDOTopUnpopulated.jpg">
+
+* Unpopulated board top view
+
+<img src="doc/assets/GDOBottom.jpg">
+
+* board bottom view
